04dce6009713b0f6aa36de194c7349912024cedc
realbenben <b.patou59@gmail.com>
2021-11-19 14:22:48 +0100

    add responsive and bg menu

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -3,15 +3,34 @@ import { FaBeer } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function Navbar() {
-  // const [toggleMenu, setToggleMenu]
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [largeur, setLargeur] = useState(window.innerWidth);
+  const toggleNavSmallScreen = () => {
+    setToggleMenu(!toggleMenu);
+  };
+  useEffect(() => {
+    const changeWidth = () => {
+      setLargeur(window.innerWidth);
+      if (window.innerWidth > 500) {
+        setToggleMenu(false);
+      }
+    };
+    window.addEventListener("resize", changeWidth);
+    return () => {
+      window.removeEventListener("resize", changeWidth);
+    };
+  }, []);
   return (
     <nav>
-      <ul className="liste">
-        <li className="items">home</li>
-        <li className="items">mon CV</li>
-        <li className="items">me contacter</li>
-      </ul>
-      <button className="btn">
+      {(toggleMenu || largeur > 500) && (
+        <ul className="liste">
+          <li className="items">home</li>
+          <li className="items">mon CV</li>
+          <li className="items">me contacter</li>
+        </ul>
+      )}
+
+      <button onClick={toggleNavSmallScreen} className="btn">
         <GiHamburgerMenu />
       </button>
     </nav>
