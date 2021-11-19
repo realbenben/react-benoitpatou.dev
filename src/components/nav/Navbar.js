e15cac7cca4e34e90a9c9ca52a5f679d7fea2ecb
realbenben <b.patou59@gmail.com>
2021-11-19 16:02:57 +0100

    add svg favicon logo

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { FaBeer } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function Navbar() {
